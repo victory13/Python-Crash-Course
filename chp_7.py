978a59fd5340b2baa7517a19e8e5ec5ec2f14b37
Nikita Malgundkar <39614104+victory13@users.noreply.github.com>
2022-03-23 00:42:57 +0530

    Update chp_7.py

--- chp_7.py
+++ chp_7.py
@@ -22,3 +22,36 @@ if int(number) % 10 == 0:
     print(f"Yes! {number} you got a multiple of 10")
 else:
     print(f"No ! {number} its not a multiple of 10")
+
+ #7-4: Pizza Toppings
+#Write a loop that prompts the user to enter a series of pizza toppings until they enter a quit value. As they enter each topping,print a message saying you’ll 
+# add that topping to their pizza.
+
+prompt = "\nWhat topping would you like on your pizza?"
+prompt += "\nEnter 'quit' when you are finished: "
+
+while True:
+    topping = input(prompt)
+    if topping != 'quit':
+        print("  I'll add " + topping + " to your pizza.")
+    else:
+        break
+
+#  7-5. Movie Tickets:
+# A movie theater charges different ticket prices depending on a person’s age. If a person is under the age of 3, the ticket is free; if they are between 3 and 12, 
+# the ticket is $10; and if they are over age 12, the ticket is $15. Write a loop in which you ask users their age, and then tell them the cost of their movie ticket.
+prompt = "How old are you?"
+prompt += "\nEnter 'quit' when you are finished. "
+
+while True:
+    age = input(prompt)
+    if age == 'quit':
+        break
+    age = int(age)
+
+    if age < 3:
+        print("  You get in free!")
+    elif age < 13:
+        print("  Your ticket is $10.")
+    else:
+        print("  Your ticket is $15.")
